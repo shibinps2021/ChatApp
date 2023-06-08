a1168f5c67e6dc811747f3c0802e38640bcc6542
Shibin P S <110965634+shibinps2021@users.noreply.github.com>
2023-06-08 23:33:38 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
-  $DB_HOST = "containers-us-west-173.railway.app";
+  $DB_HOST = "containers-us-west-58.railway.app";
   $DB_USER = "root";
-  $DB_password = "Fs3CqcT3Fyj85Hjne3uy";
+  $DB_password = "uW1bljqMvEDtGWabqYyQ";
   $DB_NAME = "railway";
-  $DB_PORT="7872"
-  $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_password, $DB_NAME,$DB_PORT);
+  $DB_PORT="6513";
+  $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_password,$DB_NAME, $DB_PORT);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
